94e44874f08fabaf42c3c1f2b0d679fa5f9ac30e
DatDX <dinh_xuan_dat@allgrow-labo.jp>
2022-03-05 21:38:14 +0700

    ChatForm with chat-engine module

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,7 +1,28 @@
+import firebase, { auth } from 'firebase';
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine';
+import { useHistory } from 'react-router-dom';
 
-export default function ChatForm() {
+const ChatForm = () => {
+  const history = useHistory()
+
+  const handleLogout = async () => {
+    await firebase.auth().signOut();
+    history.push('/');
+  }
+
   return (
-    <div>ChatForm</div>
+    <div>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <ChatEngine
+        projectId = "ed4fad7f-003e-40f3-9f9a-3f43cad82685"
+        userName = "."
+        userSecret = "."
+      />
+    </div>
   )
 }
+
+export default ChatForm;
