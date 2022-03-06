66fa749284046ffb7de3911bb4b69aa4a72caf3c
DatDX <dinh_xuan_dat@allgrow-labo.jp>
2022-03-06 09:45:26 +0700

    complete ChatForm

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,15 +1,68 @@
+import axios from 'axios';
 import firebase, { auth } from 'firebase';
-import React from 'react'
-import { ChatEngine } from 'react-chat-engine';
+import React, { useState, useEffect } from 'react'
+import { Avatar, ChatEngine } from 'react-chat-engine';
 import { useHistory } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const ChatForm = () => {
-  const history = useHistory()
+  const { user } = useAuth();
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
 
   const handleLogout = async () => {
     await firebase.auth().signOut();
     history.push('/');
   }
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: 'image/jpeg' });
+  }
+
+  useEffect(() => {
+    if(!user) {
+      history.push('/');
+      return;
+    }
+
+    axios.get('https://api.chatengine.io/users/me',{
+      headers: {
+        "project-id": "ed4fad7f-003e-40f3-9f9a-3f43cad82685",
+        "user-name": user.email,
+        "user-secret": user.uid,
+      }
+    })
+    .then(() =>{
+      setLoading(false);
+    })
+    .catch(() =>{
+      // Send user data to chatengine console
+      let formData = new FormData();
+      formData.append('email', user.email);
+      formData.append('username', user.email);
+      formData.append('secret', user.uid);
+      console.log('formData', formData);
+
+      getFile(user.photoURL)
+        .then((avatar) => {
+          formData.append('avatar', avatar, avatar.name);
+
+          axios.post('https://api.chatengine.io/users',
+            formData,
+            { headers: { "private-key": "09a9890a-e6c1-47dc-87ac-873992d1e141" }}
+          )
+          .then(() => {
+            setLoading(false);
+          })
+          .catch((error) => {console.log(error);})
+        })
+    })
+  }, [user, history]);
+
+  if( !user || loading ) return 'Loading...';
 
   return (
     <div>
@@ -17,9 +70,9 @@ const ChatForm = () => {
         <button onClick={handleLogout}>Logout</button>
       </div>
       <ChatEngine
-        projectId = "ed4fad7f-003e-40f3-9f9a-3f43cad82685"
-        userName = "."
-        userSecret = "."
+        projectID = "ed4fad7f-003e-40f3-9f9a-3f43cad82685"
+        userName = {user.email}
+        userSecret = {user.uid}
       />
     </div>
   )
